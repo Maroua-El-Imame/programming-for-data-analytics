8b7f605786b6b56e3a47442048d3978b2e0f64ff
Maroua El Imame <g00472980@atu.ie>
2025-11-08 21:28:53 +0000

    Lab 02

---
+++ my-work/csvtolist.py
@@ -0,0 +1,43 @@
+# csvtolist.py
+# This program reads in the data from 'data.csv' file and outputs each line as a list. 
+# Author : Maroua EL imame
+
+
+# 1) read data
+
+import csv
+FILENAME= "data.csv"
+DATADIR = "../my-work/"        # locate the file location using cmd, then use the following format to locate directory r"C:\Users\marou\Desktop\programming-for-data-analytics\my-work\\"
+
+
+with open (DATADIR + FILENAME, "rt") as fp:
+    reader = csv.reader(fp, delimiter=",")
+    for line in reader:
+        print (line)
+
+
+# What data type are these?
+# a List, Square Brackets [] for Lists
+
+# ['id', 'age', 'name']
+# ['1', '20', 'Joe']
+# ['2', '21', 'Mary']
+# ['3', '32', 'Fred']
+
+# 3) Modify the program to deal with the header line separately
+
+
+with open (DATADIR + FILENAME, "rt") as fp:
+    reader = csv.reader(fp, delimiter=",")
+    linecount = 0
+    for line in reader:
+        if not linecount: # first row ie header row
+            print (f"{line}\n-------------------")
+    else: # all subsequent rows
+        print (line)
+        linecount += 1
+
+
+
+
+
