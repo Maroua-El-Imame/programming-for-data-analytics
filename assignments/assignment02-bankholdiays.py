bbad68f1ceea6a975e224fa11e66fa46d850d197
Maroua El Imame <g00472980@atu.ie>
2025-11-15 01:22:57 +0000

    Assign 03 - Pie chart complete

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -1,10 +1,13 @@
 # assignment02-bankholdiays.py
 # Northern bank holidays
-# 1st part of program prints out the dates of the bank holidays that happen in northern Ireland.
+# 1. first part of program prints out the dates of the bank holidays that happen in northern Ireland.
+# 2. Second part, modified program that prints out the bank holidays which are unique to northern Ireland (i.e. do not happen elsewhere in the UK) 
+
 
 # Author: Maroua EL imame 
 
-# Import requests module library used to send HTTP requests and interact with web resources. 
+# Part 1 : 
+# Import requests module library, it is used to send HTTP requests and interact with web resources. 
 import requests
 # The URL we want to send the request to, in other words, we retrieve the data needed from the url provided
 url =" https://www.gov.uk/bank-holidays.json"
@@ -13,8 +16,7 @@ response = requests.get(url)
 # convert the website's json data to dictionary in Python
 data = response.json()
 # only get bank holidays specific to Northern Ireland
-print(data['northern-ireland']) 
+for event in data['northern-ireland']['events']:
+    print(f"{event['title']} on {event['date']}")
 
-
-# 2nd part, is modified program that prints out the bank holidays which are unique to northern Ireland (i.e. do not happen elsewhere in the UK) 
-
+# Part  
