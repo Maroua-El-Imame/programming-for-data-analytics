69404fee1a154456304867c0c4fb9ad6514e7cb6
Maroua El Imame <g00472980@atu.ie>
2025-12-07 23:50:51 +0000

    assignment2 code + markdown update&review

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -16,7 +16,46 @@ response = requests.get(url)
 # convert the website's json data to dictionary in Python
 data = response.json()
 # only get bank holidays specific to Northern Ireland
+print("\n")
+print("=== ALL Northern Ireland Bank Holidays ===")
+print("\n")
+
+
 for event in data['northern-ireland']['events']:
     print(f"{event['title']} on {event['date']}")
 
-# Part  
+# Part 1: Code explained
+# Import requets module library which is used to send HTTP requests and interact with web resources. 
+# assign a Url variable to the URL we want to send the request to, in other words, we send the request to the website and get its data the data.  
+# convert the website's json data to dictionary in Python
+# only get bank holidays specific to Northern Ireland
+# I used a for-loop to iterate through each event in the Northern Ireland events list and printed both the title and date for every bank holiday.
+
+
+# Part 2 : 
+# https://realpython.com/python-sets/
+# https://docs.python.org/3/library/stdtypes.html#set.add
+# Chat GPT & Deep seek : AI provided conceptual explanations of SET operations, it was used as a learning aid to understand:
+# Set operations (union, difference) in Python
+# Set comprehension syntax
+
+print("\n")
+print("=== UNIQUE Northern Ireland Bank Holidays ")
+print("\n")
+
+# create sets (with .add() automatic duplicate removal)
+england_set = set()
+scotland_set = set()
+
+for event in data['england-and-wales']['events']:
+    england_set.add(event['date']) 
+
+for event in data['scotland']['events']:
+    scotland_set.add(event['date'])
+
+# combine sets
+all_other_dates = england_set | scotland_set  # | is the union operator
+
+for event in data['northern-ireland']['events']:
+    if event['date'] not in all_other_dates:
+        print(f"{event['title']} on {event['date']}")
